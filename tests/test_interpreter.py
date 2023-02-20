750aec94f2ccc41b0f52097fc84266d0d8e78fdc
spikehead33 <ccspikehead33@gmail.com>
2023-02-20 23:55:03 +0800

    add unittests for interpreter

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,10 +1,25 @@
-from pythonlisp.interpreter import Interpreter
+from pythonlisp.interpreter import Env, FunctionDef, Interpreter
+
+
+def test_env():
+    root_env = Env()
+    child_env = Env(parent=root_env)
+    grand_env = Env(parent=child_env)
+    root_env.add("x", 10)
+    child_env.add("y", "This is a man")
+    assert grand_env.find("y") == "This is a man"
+    assert root_env.find("x") == 10
+    assert child_env.find("x") == 10
+    assert grand_env.find("x") == 10
+    assert grand_env.find("y") == "This is a man"
+    assert grand_env.find("k") == None
 
 
 def test_factorial():
     lisp = """
     (define fact (lambda (n) (if (= n 0) 1 (* n (fact (- n 1))))))
+    (fact 10)
     """
 
     intp = Interpreter()
-    intp.interpret(lisp)
+    assert intp.interpret(lisp) == 3628800
